7d629d6725bb47b21274b748d23cd94fa1a891b0
mistova <33962976+mistova@users.noreply.github.com>
2019-03-25 19:57:22 +0300

    Main

--- src/Android Frontend/app/src/main/java/com/work/prepmaster/MainActivity.java
+++ src/Android Frontend/app/src/main/java/com/work/prepmaster/MainActivity.java
@@ -1,13 +1,37 @@
 package com.work.prepmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button play, options, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play = findViewById(R.id.buttonPlay);
+        options = findViewById(R.id.buttonOptions);
+        profile = findViewById(R.id.buttonProfile);
+        play.setOnClickListener(this);
+        options.setOnClickListener(this);
+        profile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intentPlay = new Intent(this, PracticeActivity.class);
+        Intent intentOptions = new Intent(this, OptionsActivity.class);
+        Intent intentProfile = new Intent(this, ProfileActivity.class);
+        if (view == play)
+            startActivity(intentPlay);
+        if (view == options)
+            startActivity(intentOptions);
+        if (view == profile)
+            startActivity(intentProfile);
     }
 }
---
+++ src/Android Frontend/app/src/main/java/com/work/prepmaster/ProfileOptionsActivity.java
@@ -0,0 +1,13 @@
+package com.work.prepmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_options);
+    }
+}
