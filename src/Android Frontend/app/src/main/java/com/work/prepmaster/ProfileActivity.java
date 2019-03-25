af5b16e7f76c8da3c136702614e70d4c27a3be6f
mistova <33962976+mistova@users.noreply.github.com>
2019-03-25 20:04:36 +0300

    new activities

--- src/Android Frontend/app/src/main/java/com/work/prepmaster/ProfileActivity.java
+++ src/Android Frontend/app/src/main/java/com/work/prepmaster/ProfileActivity.java
@@ -1,13 +1,37 @@
 package com.work.prepmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button back, options, statistic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        back = findViewById(R.id.buttonProfileBack);
+        back.setOnClickListener(this);
+        options = findViewById(R.id.buttonProfileOptions);
+        options.setOnClickListener(this);
+        statistic = findViewById(R.id.buttonStatistic);
+        statistic.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intentBack = new Intent(this,MainActivity.class);
+        Intent intentOptions = new Intent(this,ProfileOptionsActivity.class);
+        Intent intentStatistic = new Intent(this,StatisticActivity.class);
+        if(view == back)
+            startActivity(intentBack);
+        if(view == options)
+            startActivity(intentOptions);
+        if(view == statistic)
+            startActivity(intentStatistic);
     }
 }
--- src/Android Frontend/app/src/main/java/com/work/prepmaster/ProfileOptionsActivity.java
+++ src/Android Frontend/app/src/main/java/com/work/prepmaster/ProfileOptionsActivity.java
@@ -1,13 +1,27 @@
 package com.work.prepmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileOptionsActivity extends AppCompatActivity {
+public class ProfileOptionsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_options);
+        back = findViewById(R.id.buttonProfileOptionsBack);
+        back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intentBack = new Intent(this,ProfileActivity.class);
+        if(view == back)
+            startActivity(intentBack);
     }
 }
