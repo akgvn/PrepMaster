46d52c38be8a098f4363bbfd66f1d6e0b1d187f2
mistova <33962976+mistova@users.noreply.github.com>
2019-03-25 20:01:57 +0300

    new Activities

--- src/Android Frontend/app/src/main/java/com/work/prepmaster/PracticeActivity.java
+++ src/Android Frontend/app/src/main/java/com/work/prepmaster/PracticeActivity.java
@@ -1,13 +1,27 @@
 package com.work.prepmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PracticeActivity extends AppCompatActivity {
+public class PracticeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+        back = findViewById(R.id.buttonPracticeBack);
+        back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intentBack = new Intent(this, MainActivity.class);
+        if(view == back)
+            startActivity(intentBack);
     }
 }
