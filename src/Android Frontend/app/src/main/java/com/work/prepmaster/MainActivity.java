b47fa90b54237eacffe13cdb97e399ba851470ec
vrlcnn <vuralcansisman1@gmail.com>
2019-03-27 00:47:28 +0300

    activity_login.xml tasarımı değişti komple değiştirildi. activity_main.xml içine geçici bir buton konuldu ve tasarım kaba taslağı yapıldı. MainActiviyu profile değişkeninin tipi ImageVıew olarak değiştirildi.

--- src/Android Frontend/app/src/main/java/com/work/prepmaster/MainActivity.java
+++ src/Android Frontend/app/src/main/java/com/work/prepmaster/MainActivity.java
@@ -5,21 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button play, options, profile;
+    private Button play, options, login_page;
+    private ImageView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        play = findViewById(R.id.buttonPlay);
-        options = findViewById(R.id.buttonOptions);
-        profile = findViewById(R.id.buttonProfile);
+        play       = findViewById(R.id.buttonPlay);
+        options    = findViewById(R.id.buttonOptions);
+        profile    = findViewById(R.id.buttonProfile);
+        login_page = findViewById( R.id.login_page );
+
         play.setOnClickListener(this);
         options.setOnClickListener(this);
         profile.setOnClickListener(this);
+        login_page.setOnClickListener(this);
+
     }
 
     @Override
@@ -27,11 +34,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intentPlay = new Intent(this, PracticeActivity.class);
         Intent intentOptions = new Intent(this, OptionsActivity.class);
         Intent intentProfile = new Intent(this, ProfileActivity.class);
+        Intent intentLogin = new Intent(this, LoginActivity.class);
         if (view == play)
             startActivity(intentPlay);
         if (view == options)
             startActivity(intentOptions);
         if (view == profile)
             startActivity(intentProfile);
+        if( view == login_page )
+            startActivity(intentLogin);
+
     }
 }
