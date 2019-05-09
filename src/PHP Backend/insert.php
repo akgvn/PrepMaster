6cedc9f7ab2a8d599955fada8babe61d119514fd
Kayhan Arıcan <32819375+MKayhan8@users.noreply.github.com>
2019-05-09 22:59:15 +0300

    Update insert.php
    Sending words app update..

--- src/PHP Backend/insert.php
+++ src/PHP Backend/insert.php
@@ -1,35 +1,32 @@
 <?php
-
 	// INSERT INTO table_name SET col1 = value1;
 	
 	/*$db->query('INSERT INTO fill_blanks SET word="range(n)",meaning="variety,kinds",sentence="I saw a range of colours/patterns on the carpet"');
-
 	$db->query('INSERT INTO fill_blanks SET word="principal(n)",meaning="manager",sentence="There are seven theachers and a principal at the school"');
 	$db->query('INSERT INTO fill_blanks SET word="encourage(v)",meaning="persuade",sentence="My parents encourage me when thing were not going well at school"');
 	$db->query('INSERT INTO fill_blanks SET word="enhance(v)",meaning="improve",sentence="I need to enhance my English ability to pass exam."');
 	$db->query('INSERT INTO fill_blanks SET word="evidence(n)",meaning="something that makes you believe that something is true or exists.",sentence="There is no scientific evidence that the drug is addictive."');
 	$db->query('INSERT INTO fill_blanks SET word="expand(v)",meaning="to become larger in size,number,or,amount,or to make something become larger.",sentence="Sydney s popilation expanded rapidly in the 1960 s."');
-
 */
+	require_once 'connect.php';
 	
 	$x1 = rand(1,6);
-
 	$sorgu = $db -> prepare('SELECT * FROM fill_blanks WHERE id =?' );
 	$sorgu->execute([
 		$x1
 	]);
 	$words = $sorgu->fetchALL(PDO::FETCH_ASSOC);
 	//print_r($words);
-
 	$array = array();
 	
 	$array[] =$words[0]['sentence'];
 	$array[] =$words[0]['word'];
-	$array[] =$words[0]['meaning'];
 
+	$array[] =$words[0]['meaning'];
 	$check=array();
 	$check[]=$x1;
 	$i=0;
+	$metin = "";
 	do{
 		$temp=$i;
 		$y1 = rand(1,6);
@@ -41,20 +38,18 @@
 				]);
 			$words2 = $sorgu->fetchALL(PDO::FETCH_ASSOC);
 			$array[] =$words2[0]['word'];
+			
 			$check[]=$y1;
 			$i++;
 		}
 		
-
 	}while($i<3);
-
-		echo json_encode(  $array );
-			
-
-
+	$metin .= '{"sentence":"' . $array[0] . '"' . ',"msg1":' . '"' .$array[1] . '"' . ',"msg2":'. '"' .$array[2].'"'.   ',"msg3":'. '"' .$array[3].'"'.  ',"msg4":'. '"' .$array[4].'"'. ',"msg5":'. '"' .$array[5].'"'. '}';
+		//echo json_encode(  $array );
+			echo $metin;
 		
 	
-	print_r($array);
+	//print_r($array);
 	
 	/*
 	function turkce($php){
@@ -77,7 +72,6 @@
 	
 	
 	
-
 	
 	
 ?>
