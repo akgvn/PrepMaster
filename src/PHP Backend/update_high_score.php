6c67a8bc06c0c40ae21716de5192f308b97c1d3a
vrlcnn <vuralcansisman1@gmail.com>
2019-05-21 16:03:08 +0300

    Update update_high_score.php
    1 2 ufak değişiklik

--- src/PHP Backend/update_high_score.php
+++ src/PHP Backend/update_high_score.php
@@ -1,57 +1,68 @@
 <?php
-    
-try{
-    $db = new PDO('mysql:host=localhost;dbname=u8250606_chard;charset=utf8','u8250606_user22C','DUpj07B2');
-}catch(PDOException $e){
-    echo $e -> getMessage();
-}
-        
-
-$id    = trim( $_GET[ "user_id" ] );
-$name  = trim( $_GET[ "user_name" ] );
-$score = trim( $_GET[ "user_score" ] );
-$true  = trim( $_GET[ "user_true" ] );
-$false = trim( $_GET[ "user_false" ] );
-
-
-if( $_GET )
-{
-
-    if( isset( $id ) && isset( $name ) && isset( $score ) && isset( $true ) && isset($false ) )
-    {
-
-        if( !empty( $id ) && !empty( $name ) && !empty( $score ) && !empty( $true ) && !empty( $false ) )
-        {
-
-            $query = $db->prepare("UPDATE users SET
-            user_score = :new_score , user_true_ans = :new_true , user_false_ans = :new_false
-            WHERE user_id = :id and user_nick_name = :name");
-            $update = $query->execute(array(
-                "new_score" => $score,
-                "new_true" => $true,
-                "new_false" => $false,
-                "id" => $id,
-                "name" => $name
-            ));
-
-            if( $update )
-                echo json_encode( array( "ans" => 3 ) );//UPDATE EDİLDİ
-            else
-                echo json_encode( array( "ans" => 2 ) );//UPDATE OLMADI
-
+     
+    try{
+         $db = new PDO('mysql:host=localhost;dbname=u8250606_chard;charset=utf8','u8250606_user22C','DUpj07B2');
+        }catch(PDOException $e){
+             echo $e -> getMessage();
         }
-        else
-        {
-            echo json_encode( array( "ans" => 1 ) );//BOŞ BİR DEĞİŞKEN GÖNDEREMEZSİNİZ
-        }
-
-    }
-    else
-    {
-        echo json_encode( array( "ans" => 0 ) );//TANIMSIZ DEĞİŞKEN.
-    }
-
-}
-
+         
+        $name = trim( $_GET[ "user_name" ] );
+        $score = trim( $_GET[ "user_score" ] );
+        $true = trim( $_GET[ "user_true" ] );
+        $false = trim( $_GET[ "user_false" ] );
         
-?>
+        
+        if( $_GET )
+        {
+            
+             if( isset( $name ) && isset( $score ) && isset( $true ) && isset($false ) )
+             {
+                
+                 if( !empty( $name ) && !empty( $score ) && !empty( $true ) && !empty( $false ) )
+                 {
+                    
+                    $cek = $db -> prepare( "SELECT * FROM users WHERE user_nick_name=?" );
+                    $cek -> execute( array( $name ) );
+
+                    foreach( $cek as $yaz )
+                    {
+
+                        $score = $score + $yaz[ "user_score" ];
+
+                        $true  = $true + $yaz[ "user_true_ans" ];
+
+                        $false = $false + $yaz[ "user_false_ans" ];
+
+                    }
+
+                     $query = $db->prepare("UPDATE users SET
+                     user_score = :new_score , user_true_ans = :new_true , user_false_ans = :new_false
+                     WHERE user_nick_name = :name");
+                     $update = $query->execute(array(
+                     "new_score" => $score,
+                     "new_true" => $true,
+                     "new_false" => $false,
+                     "name" => $name
+                     ));
+                    
+                     if( $update )
+                     echo json_encode( array( "ans" => 3 ) );//UPDATE EDİLDİ
+                     else
+                     echo json_encode( array( "ans" => 2 ) );//UPDATE OLMADI
+                    
+                 }
+                 else
+                 {
+                     echo json_encode( array( "ans" => 1 ) );//BOŞ BİR DEĞİŞKEN GÖNDEREMEZSİNİZ
+                 }
+                
+             }
+             else
+             {
+                 echo json_encode( array( "ans" => 0 ) );//TANIMSIZ DEĞİŞKEN.
+             }
+            
+        }
+        
+         
+        ?>
