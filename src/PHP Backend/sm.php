99c96a1a0fe398cddcfd513164f1f2dce9e261bd
akgvn <akgvn@yandex.com>
2019-05-21 19:34:04 +0300

    Progress on scheduling part.

--- src/PHP Backend/sm.php
+++ src/PHP Backend/sm.php
@@ -20,9 +20,11 @@ if (isset($_POST["answered"]) && isset($_POST["user_id"]) && isset($_POST["word_
     // Schedule reminding time!
 
     try {
-        $stmt = $db->prepare("SELECT * FROM schedule WHERE user_id=:uid AND word_id=wid");
+        // Get all rows w/ this user & word, which hasn't been asked yet.
+        $stmt = $db->prepare("SELECT * FROM schedule WHERE user_id=:uid AND word_id=wid AND asked=:ask");
         $stmt->bindparam(":uid", $user_id);
         $stmt->bindparam(":wid", $word_id);
+        $stmt->bindparam(":ask", 0);
 
         $stmt->execute();
     } catch (PDOException $e) {
@@ -32,15 +34,38 @@ if (isset($_POST["answered"]) && isset($_POST["user_id"]) && isset($_POST["word_
 
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if (!(count($rows) > 0)) {
+    if (count($rows) == 0) {
         // This word wasn't scheduled before.
-        
+
         $efactor = 2.5;
         $repeat = 0;
 
-    } else {
+    } else if (count($rows) == 1) {
         // TODO Prepare the variables for INSERTing to DB.
+
+        try {
+            // Update the scheduled row as asked.
+            $stmt = $db->prepare("UPDATE schedule SET asked=1 WHERE qid=:qi");
+            $stmt->bindparam(":qi", $rows["qid"]);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit; // FIXME Send error code?
+        }
+
+        $efactor = easiness($rows["efactor"], $grade);
+        $intrvl = interval($rows["repeat_time"], $efactor, $rows["old_interval"]); // Number of days from now.
+        
+        // TODO find the date to schedule here.
+    } else {
+        // TODO Shouldn't happen. Add error for this.
     }
+
+    // TODO Do the INSERTION
+
+    $stmt = $db->prepare("INSERT INTO schedule(word_id, user_id, date, efactor, repeat_time, old_interval) 
+    VALUES(:wid, :uid, :dte, :ef, :rt, :oi)");
+    
 
 } else {
     echo "Invalid request!";
