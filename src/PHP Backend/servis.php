890cb0aa058a0352a67e49c2707d36f4c05c9812
akgvn <akgvn@yandex.com>
2019-05-22 15:34:47 +0300

    Deleted unneeded comments

--- src/PHP Backend/servis.php
+++ src/PHP Backend/servis.php
@@ -1,13 +1,5 @@
 <?php
-	// INSERT INTO table_name SET col1 = value1;
-	
-	/*$db->query('INSERT INTO fill_blanks SET word="range(n)",meaning="variety,kinds",sentence="I saw a range of colours/patterns on the carpet"');
-	$db->query('INSERT INTO fill_blanks SET word="principal(n)",meaning="manager",sentence="There are seven theachers and a principal at the school"');
-	$db->query('INSERT INTO fill_blanks SET word="encourage(v)",meaning="persuade",sentence="My parents encourage me when thing were not going well at school"');
-	$db->query('INSERT INTO fill_blanks SET word="enhance(v)",meaning="improve",sentence="I need to enhance my English ability to pass exam."');
-	$db->query('INSERT INTO fill_blanks SET word="evidence(n)",meaning="something that makes you believe that something is true or exists.",sentence="There is no scientific evidence that the drug is addictive."');
-	$db->query('INSERT INTO fill_blanks SET word="expand(v)",meaning="to become larger in size,number,or,amount,or to make something become larger.",sentence="Sydney s popilation expanded rapidly in the 1960 s."');
-*/
+
 	require_once 'connect.php';
 	
 	$x1 = rand(1,6);
