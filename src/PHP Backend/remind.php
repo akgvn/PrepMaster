5ab86805f198a7d05336ca04713d46a15ce2b4f2
akgvn <akgvn@yandex.com>
2019-05-22 15:37:33 +0300

    Minor progress

--- src/PHP Backend/remind.php
+++ src/PHP Backend/remind.php
@@ -7,9 +7,24 @@ require_once "supermemo.php";
 
 // App requests questions to ask to the user.
 if (isset($_POST["user_id"])) {
-    // TODO
+
+    $user_id = $_POST["user_id"];
+    $asked = 0;
+
+    try {
+        // Get all scheduled questions for this user, which hasn't been asked yet.
+        $stmt = $db->prepare("SELECT * FROM schedule WHERE user_id=:uid AND asked=:ask");
+        $stmt->bindparam(":uid", $user_id);
+        $stmt->bindparam(":ask", $asked);
+
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit; // FIXME Send error code?
+    }
+
+    // TODO Check for questions and return them!
+
 } else {
     echo "Invalid request!"; // FIXME Send error code with JSON?
 }
-
-?>
